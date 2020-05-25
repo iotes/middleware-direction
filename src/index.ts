b2a09fcfb3d6e781fa5d55f90f11dc728161ee44
Seamus Foley <seamus.foley@icloud.com>
2020-05-26 08:29:40 +1200

    corrected indentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Direction, State, Middleware } from '@iotes/core'
 
 export const direction = (d: Direction): Middleware => (state: State) => {
-    if (d === undefined || d === null) return state
-    if (d === 'B') return state
-    return Object.entries(state).reduce((a, [key, value]) => {
-        const shouldInclude = (d === value['@@iotes_direction'])
-        return shouldInclude ? { ...a, [key]: value } : {}
-    }, {})
+  if (d === undefined || d === null) return state
+  if (d === 'B') return state
+  return Object.entries(state).reduce((a, [key, value]) => {
+    const shouldInclude = (d === value['@@iotes_direction'])
+    return shouldInclude ? { ...a, [key]: value } : {}
+  }, {})
 }
